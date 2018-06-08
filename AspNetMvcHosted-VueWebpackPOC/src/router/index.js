f06bab6a748a6ebf233f0317e471a877ab455f49
Steve Tipton <slaatipton26@gmail.com>
2018-06-08 15:32:24 -0400

    Add router config for Todos component

--- AspNetMvcHosted-VueWebpackPOC/src/router/index.js
+++ AspNetMvcHosted-VueWebpackPOC/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Todos from '@/components/Todos'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/todos',
+      name: 'Todos',
+      component: Todos
     }
   ]
 })
